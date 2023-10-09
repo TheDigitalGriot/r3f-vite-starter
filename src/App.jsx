f2fe37ab1996ea3419cf24b0ac1ed33ca01b4756
TheDigitalGriot <132814236+TheDigitalGriot@users.noreply.github.com>
2023-10-08 23:54:56 -0400

    add theatrejs setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,75 @@
-import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Gltf, Scroll, ScrollControls, useScroll } from "@react-three/drei";
+import { getProject, val } from "@theatre/core";
 
-function App() {
+import {
+  SheetProvider,
+  PerspectiveCamera,
+  useCurrentSheet,
+} from "@theatre/r3f";
+
+import { Interface } from "./components/Interface";
+export default function App() {
+  const [section, setSection] = useState(0);
+  const [currentSection, setCurrentSection] = useState(0);
+  const [started, setStarted] = useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [studioOpened, setStudioOpened] = useState(false);
+  const [interviewOpened, setInterviewOpened] = useState(false);
+
+  // const sheet = getProject("ProjectName").sheet("Scene");
+  const sheet = getProject("ProjectName", {state: gbStudioState}).sheet("Scene");
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
+      <ScrollControls pages={5}>
+        {/* Canvas contents in here will *not* scroll, but receive useScroll! */}
+        
+        {/* <SomeModel /> */}
+        <SheetProvider sheet={sheet}>
+          <Scene />
+        </SheetProvider>
+        <Scroll html>
+          {/* DOM contents in here will scroll along */}
+
+          <Interface setSection={setSection} />
+        </Scroll>
+        
+      </ScrollControls>
     </Canvas>
   );
 }
 
-export default App;
+function Scene() {
+  const sheet = useCurrentSheet();
+  const scroll = useScroll();
+
+  // our callback will run on every animation frame
+  useFrame(() => {
+    // the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length);
+    // update the "position" of the playhead in the sequence, as a fraction of its whole length
+    sheet.sequence.position = scroll.offset * sequenceLength;
+  });
+
+  const bgColor = "#3772b8";
+
+  return (
+    <>
+      <color attach="background" args={[bgColor]} />
+      <fog attach="fog" color={bgColor} near={-4} far={15} />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[-5, 5, -5]} intensity={1.5} />
+      <Gltf src="/Model.glb" castShadow receiveShadow />
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 0, 0]}
+        fov={90}
+        near={0.1}
+        far={70}
+      />
+    </>
+  );
+}
---
+++ src/components/Interface.jsx
@@ -0,0 +1,169 @@
+import { motion } from "framer-motion";
+import { useAtom } from "jotai";
+
+const Section = (props) => {
+  const { children, mobileTop } = props;
+
+  return (
+    <motion.section
+      className={`
+  h-screen w-screen p-8 max-w-screen-2xl mx-auto
+  flex flex-col items-start
+  ${mobileTop ? "justify-start md:justify-center" : "justify-center"}
+  `}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export const Interface = (props) => {
+  const { setSection } = props;
+  return (
+    <div className="flex flex-col items-center w-screen">
+      <Section1 setSection={setSection} />
+      <Section2 />
+      <ContactSection />
+    </div>
+  );
+};
+
+const Section1 = (props) => {
+  const { setSection } = props;
+  return (
+    <Section mobileTop>
+      <h1 className="text-4xl md:text-6xl font-extrabold leading-snug mt-8 md:mt-0">
+        This is
+        <br />
+        <span className="bg-white px-1 italic">Section 1</span>
+      </h1>
+      <motion.p
+        className="text-lg text-gray-600 mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
+      >
+        This is the content for section 1
+        <br />
+        Put your content here
+      </motion.p>
+    </Section>
+  );
+};
+
+const Section2 = () => {
+    return (
+        <Section mobileTop>
+          <h1 className="text-4xl md:text-6xl font-extrabold leading-snug mt-8 md:mt-0">
+            This is
+            <br />
+            <span className="bg-white px-1 italic">Section 2</span>
+          </h1>
+          <motion.p
+            className="text-lg text-gray-600 mt-4"
+            initial={{
+              opacity: 0,
+              y: 25,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 1,
+              delay: 1.5,
+            }}
+          >
+            This is the content for section 2
+            <br />
+            Put your content here
+          </motion.p>
+        </Section>
+      );
+};
+
+const ContactSection = () => {
+  const [state, handleSubmit] = useForm("mayzgjbd");
+  return (
+    <Section>
+      <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white bg-opacity-50 w-96 max-w-full">
+        {state.succeeded ? (
+          <p className="text-gray-900 text-center">Thanks for your message !</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name" className="font-medium text-gray-900 block mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+            />
+            <label
+              htmlFor="email"
+              className="font-medium text-gray-900 block mb-1 mt-8"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+            />
+            <ValidationError
+              className="mt-1 text-red-500"
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <label
+              htmlFor="email"
+              className="font-medium text-gray-900 block mb-1 mt-8"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+            />
+            <ValidationError
+              className="mt-1 text-red-500"
+              errors={state.errors}
+            />
+            <button
+              disabled={state.submitting}
+              className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 "
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+    </Section>
+  );
+};
